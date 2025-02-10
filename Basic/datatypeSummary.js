3af635ecc62eb2e40268cba48cdee307760d3b94
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-10 13:45:10 +0000

    Datatype Summary

---
+++ Basic/datatypeSummary.js
@@ -0,0 +1,25 @@
+//Data are mainly  two types
+// 1 -> primitive datatype:call by value
+
+// 7 types: string , Number, Boolean, null, undefined, symbol(use in uniquely classify), bigInt
+
+
+// 2 -> Non-primitive datatype
+//Reference type(non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Shaktiman","naagraj","Doga"];
+let myObj = {
+    name : "Kartik",
+    age : 20
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+
+
+
+// JavaScript is a dynamically typed language, which means that the type of a variable is determined by its value at runtime and can change throughout the program.
