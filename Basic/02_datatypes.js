be88b07a21ba774e8620b16fae813f6c666909d9
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-10 12:59:19 +0000

    Data type

---
+++ Basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using nodeja, not browser
+
+console.log("Kartik")
+
+let name = "Kartik"
+let age = 20
+let isLoggedIn = false
+let state;
+//number => 2 to the power of 53
+//bigInt
+//stirng =>"write in double courtation"
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique 
+
+//object
+
+console.log(typeof "Kartik")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
