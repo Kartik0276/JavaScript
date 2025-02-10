933025037fa52481b114dfb4cbf5dbe0cf72e0f7
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-10 12:59:52 +0000

    data type

---
+++ Basic/01_variable.js
@@ -0,0 +1,7 @@
+let name = "Kartik"
+const password = 23697
+cityName = "Kolkata"
+let accountState
+console.table([name, password, cityName,accountState])
+
+//Don't use var because of issue in block scope and functional scope
