927cd298ecb438653cb42b67161d9fd3cf275381
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-11 16:20:53 +0000

    Scopes in js

--- 03_Basic/02_scopes.js
+++ 03_Basic/02_scopes.js
@@ -9,6 +9,28 @@ console.log(d) //this is why we don't use var because it not has block scope
 // console.log(e)
 
 const array = [34, 54, 54, 90]
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+function one(){
+    const username = "kartik"
+    function two(){
+        console.log(username)
+    }
+    console.log(username)
+    two()
 }
+
+one()
+
+
+// ---------------- intersting -------------
+function addone(num){
+    return num+1
+}
+console.log(addone(5))
+
+// const addTwo = function(num){
+//     return num+2
+// }
