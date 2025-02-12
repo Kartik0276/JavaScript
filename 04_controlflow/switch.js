da70136f6937ae327b5d352fe5677236549d2c13
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-12 13:33:39 +0000

    Switch case in js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,38 @@
+//-----------------------switch------------
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 15
+switch(month){
+    case 1: console.log("This is january")
+    break
+    case 2:console.log("This is February")
+    break
+    case 3: console.log("This is March")
+    break
+    case 4:console.log("This is April")
+    break
+    case 5: console.log("This is May")
+    break
+    case 6:console.log("This is June")
+    break
+    case 7: console.log("This is Jully")
+    break
+    case 8:console.log("This is August")
+    break
+    case 9:console.log("This is September")
+    break
+    case 10:console.log("This is October")
+    break
+    case 11:console.log("This is November")
+    break
+    default:console.log("This is December")
+    break
+
+}
