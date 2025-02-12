8c642448a802647508295e135b7e6a6e46d7578a
Kartik Maity <128496732+Kartik0276@users.noreply.github.com>
2025-02-12 14:29:13 +0000

    while loop, Do while loop

---
+++ 05_Loops/two.js
@@ -0,0 +1,31 @@
+//while loop
+
+// while (condition) {
+    
+// }
+
+let i = 1
+while(i < 10){
+    console.log(`Value is ${i}`)
+    i++
+}
+
+let myArray = ["Flash", "Batman", "Superman"]
+let index = 0;
+while(index < myArray.length){
+    console.log(`value is ${myArray[index]}`)
+    index++
+}
+
+//---------------do while loop----------------
+// do {
+    
+// } while (condition);
+
+let score = 1;
+do{
+    console.log(`Score is ${score}`)
+    score++;
+}while (score <= 10);
+
+
